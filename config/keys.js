58460d9eb18f65d5d369097ad600f4cf5aa4df09
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-02 11:50:04 -0700

    added passport

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://dbUser:dbUser@cluster0-viqf0.mongodb.net/bank?retryWrites=true&w=majority"
+  mongoURI: "mongodb+srv://dbUser:dbUser@cluster0-viqf0.mongodb.net/bank?retryWrites=true&w=majority",
+  secretOrKey: "secret"
 };
