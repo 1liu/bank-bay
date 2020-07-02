65afc0b993e542890dd8e2dd441833ca515d053c
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-02 12:14:09 -0700

    fixed file name issue

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
+
 const app = express();
 require("dotenv").config();
 // Bodyparser middleware
@@ -20,5 +23,11 @@ mongoose
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+app.use("/api/users", users);
+
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server is running at port: ${port} !`));
